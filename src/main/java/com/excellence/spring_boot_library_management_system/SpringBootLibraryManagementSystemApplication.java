3a1bff683d9041f80733e250602b03649b5afb10
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-17 10:32:23 +0530

    base-code-generated

---
+++ src/main/java/com/excellence/spring_boot_library_management_system/SpringBootLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_boot_library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/spring_boot_library_management_system/SpringBootLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_boot_library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
